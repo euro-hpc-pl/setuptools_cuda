17d0afb9882cc7691649a51e0d9bdefc16f35921
Konrad Jałowiecki <dexter2206@gmail.com>
2023-02-14 11:34:39 +0100

    Fix typo

--- setuptools_cuda/extension.py
+++ setuptools_cuda/extension.py
@@ -5,6 +5,6 @@ from setuptools import Extension
 class CudaExtension(Extension):
     """CUDA-enabled extension."""
 
-    def __init__(self, name, source, extra_nvcc_args=None, *args, **kwargs):
-        super().__init__(name, source, *args, **kwargs)
+    def __init__(self, name, sources, extra_nvcc_args=None, *args, **kwargs):
+        super().__init__(name, sources, *args, **kwargs)
         self.extra_nvcc_args = [] if extra_nvcc_args is None else extra_nvcc_args
