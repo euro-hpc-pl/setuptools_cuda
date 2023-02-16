f7ad0980a8ba0478f8365a14c520a20fc50a9cff
Konrad Jałowiecki <dexter2206@gmail.com>
2023-02-16 22:53:51 +0100

    Add numpy build requirement and numpy include dirs

--- examples/02_thrust/setup.py
+++ examples/02_thrust/setup.py
@@ -1,4 +1,5 @@
 """Setup module for the `example` package."""
+import numpy as np
 from setuptools import setup
 
 from setuptools_cuda import CudaExtension
@@ -8,6 +9,7 @@ setup(
         CudaExtension(
             name="thrust",
             sources=["thrustcu/thrustcu.pyx", "thrustcu/thrustcu_impl.cu"],
+            include_dirs=[np.get_include()],
         ),
     ],
 )
