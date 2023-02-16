b175b65b0bdf8792ebc902bec4e8bde683f89cef
Konrad Jałowiecki <dexter2206@gmail.com>
2023-02-16 09:18:55 +0100

    Add default cuda libraries to be linked when building CUDA Extensions

--- setuptools_cuda/extension.py
+++ setuptools_cuda/extension.py
@@ -1,10 +1,13 @@
 """Definition of CUDA-enabled extensions."""
 from setuptools import Extension
 
+DEFAULT_CUDA_LIBS = ["cudart"]
+
 
 class CudaExtension(Extension):
     """CUDA-enabled extension."""
 
     def __init__(self, name, sources, extra_nvcc_args=None, *args, **kwargs):
         super().__init__(name, sources, *args, **kwargs)
+        self.libraries += DEFAULT_CUDA_LIBS
         self.extra_nvcc_args = [] if extra_nvcc_args is None else extra_nvcc_args
