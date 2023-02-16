b9c6124a2bb87948b2c5d775bada799e7cc886f3
Konrad Jałowiecki <dexter2206@gmail.com>
2023-02-16 17:54:46 +0100

    Remove separate n argument

--- examples/01_basic/saxpycu/saxpy.pyx
+++ examples/01_basic/saxpycu/saxpy.pyx
@@ -7,5 +7,5 @@ ctypedef fused real:
 cdef extern from "saxpy_impl.h":
     void saxpy_wrapper[T](T a, T * x, T * y, int n, int numThreads, int numBlocks);
 
-def saxpy(real a, real[::1] x, real[::1] y, int n, int num_threads, int num_blocks):
-    saxpy_wrapper(a, &x[0], &y[0], n, num_threads, num_blocks)
+def saxpy(real a, real[::1] x, real[::1] y, int num_threads, int num_blocks):
+    saxpy_wrapper(a, &x[0], &y[0], len(x), num_threads, num_blocks)
