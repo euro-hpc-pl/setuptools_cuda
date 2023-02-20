6712442caf0ee4b1de6ae64c53b429a1083768a9
Konrad Jałowiecki <dexter2206@gmail.com>
2023-02-20 01:40:36 +0100

    Remove implicit cythonization + remove mypy from pre-commit hook

--- examples/02_thrust/setup.py
+++ examples/02_thrust/setup.py
@@ -1,15 +1,18 @@
 """Setup module for the `example` package."""
 import numpy as np
+from Cython.Build import cythonize
 from setuptools import setup
 
 from setuptools_cuda import CudaExtension
 
 setup(
-    cuda_extensions=[
-        CudaExtension(
-            name="thrust",
-            sources=["thrustcu/thrustcu.pyx", "thrustcu/thrustcu_impl.cu"],
-            include_dirs=[np.get_include()],
-        ),
-    ],
+    cuda_extensions=cythonize(
+        [
+            CudaExtension(
+                name="thrust",
+                sources=["thrustcu/thrustcu.pyx", "thrustcu/thrustcu_impl.cu"],
+                include_dirs=[np.get_include()],
+            ),
+        ]
+    ),
 )
