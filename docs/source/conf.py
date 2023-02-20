ad48a1f17069def7e1489366b5279ef10cc745df
Konrad Jałowiecki <dexter2206@gmail.com>
2023-02-20 01:41:15 +0100

    Apply black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,24 +6,23 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'setuptools-cuda'
-copyright = '2023, Konrad Jałowiecki'
-author = 'Konrad Jałowiecki'
+project = "setuptools-cuda"
+copyright = "2023, Konrad Jałowiecki"
+author = "Konrad Jałowiecki"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ["myst_parser"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'pydata_sphinx_theme'
-html_static_path = ['_static']
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
 
-myst_enable_extensions = ["colon_fence"]
+myst_enable_extensions = ["colon_fence", "dollarmath"]
